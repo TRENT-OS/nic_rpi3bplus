92604bffee9d3678e5ebee5b059a44d7c4070796
Sebastian Eckl <sebastian.eckl@hensoldt-cyber.com>
2020-07-21 13:47:53 +0200

    [DEV] SEOS-1541 test only send performance on driver level

--- ethernetDriver.c
+++ ethernetDriver.c
@@ -72,26 +72,34 @@ int run()
 	}
 
 	uint8_t* Buffer = (uint8_t*)dma_alloc(DMA_PAGE_SIZE, DMA_ALIGNEMENT);
-	size_t receivedLength = 0;
+	// size_t receivedLength = 0;
+
+	static const char fix_packet[1514] = {0x00,0xE0,0x4C,0x06,0x30,0xD4,0xB8,0x27,0xEB,0x52,0x4A,0x44,0x08,0x00,0x45,0x00,0x05,0xDC,0x91,0xC4,0x40,0x00,0x40,0x06,0x8F,0x43,0x0A,0x00,0x00,0x0B,0x0A,0x00,0x00,0x0A,0x15,0xB3,0xA7,0x02,0x21,0x1E,0x29,0xA4,0xB2,0x0C,0x53,0xF1,0x90,0x18,0x3A,0x58,0x56,0x48,0x00,0x00,0x03,0x03,0x00,0x08,0x0A,0x00,0x00,0x02,0x3D,0xCB,0x49,0x14,0x23,0x01,0x01};
 
 	while(true)
 	{
-		if(*POINTER_TO_LEN_IN_DATAPORT == 0)
-		{
-			if(USPiReceiveFrame (Buffer, &receivedLength))
-			{
-				if(receivedLength > MAX_READ_DATA_LENGTH)
-				{
-					Debug_LOG_WARNING("The max length of the data is %u, but the length of the read data is %u",
-											MAX_READ_DATA_LENGTH, receivedLength);
-				}
+		// if(*POINTER_TO_LEN_IN_DATAPORT == 0)
+		// {
+		// 	if(USPiReceiveFrame (Buffer, &receivedLength))
+		// 	{
+		// 		if(receivedLength > MAX_READ_DATA_LENGTH)
+		// 		{
+		// 			Debug_LOG_WARNING("The max length of the data is %u, but the length of the read data is %u",
+		// 									MAX_READ_DATA_LENGTH, receivedLength);
+		// 		}
 
-				memcpy((((uint8_t*)nic_port_to) + *POINTER_TO_LEN_IN_DATAPORT), Buffer, receivedLength);
-				*POINTER_TO_LEN_IN_DATAPORT += receivedLength;
+		// 		memcpy((((uint8_t*)nic_port_to) + *POINTER_TO_LEN_IN_DATAPORT), Buffer, receivedLength);
+		// 		*POINTER_TO_LEN_IN_DATAPORT += receivedLength;
 
-				nic_event_hasData_emit();
-			}
-		}
+		// 		nic_event_hasData_emit();
+		// 	}
+		// }
+	}
+
+	if (!USPiSendFrame(fix_packet, 1514))
+	{
+        Debug_LOG_ERROR("USPiSendFrame failed");
+		return OS_ERROR_ABORTED;
 	}
 
 	dma_free(Buffer, DMA_ALIGNEMENT);

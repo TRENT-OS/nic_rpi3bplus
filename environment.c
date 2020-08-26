54fe5aec4324106f803163eeba7c40f4a91d6186
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-08-26 11:26:34 +0200

    [DEV] Adapt to TimeServer updates

--- environment.c
+++ environment.c
@@ -18,105 +18,120 @@
 #include <camkes.h>
 #include <camkes/dma.h>
 
+static const if_OS_Timer_t timer =
+    IF_OS_TIMER_ASSIGN(
+        timeServer_rpc,
+        timeServer_notify);
+
 /* Environment functions -------------------------------------------------------------*/
-void *dma_alloc (unsigned nSize, unsigned alignement)
+void* dma_alloc (unsigned nSize, unsigned alignement)
 {
-	return camkes_dma_alloc(nSize, alignement);
+    return camkes_dma_alloc(nSize, alignement);
 }
 
-void dma_free (void *pBlock, unsigned alignement)
+void dma_free (void* pBlock, unsigned alignement)
 {
-	camkes_dma_free(pBlock, alignement);
+    camkes_dma_free(pBlock, alignement);
 }
 
-uintptr_t dma_getPhysicalAddr(void *ptr)
+uintptr_t dma_getPhysicalAddr(void* ptr)
 {
-	return camkes_dma_get_paddr(ptr);
+    return camkes_dma_get_paddr(ptr);
 }
 
-void LogWrite (const char *pSource, unsigned Severity, const char *pMessage, ...)
+void LogWrite (const char* pSource, unsigned Severity, const char* pMessage,
+               ...)
 {
-	va_list args;
-	va_start (args, pMessage);
+    va_list args;
+    va_start (args, pMessage);
 
-	switch (Severity)
-	{
-	case USPI_LOG_ERROR:
-		printf("\n ERROR %s: - ", pSource);
-		break;
-	case USPI_LOG_WARNING:
-		printf("\n WARNING %s: - ", pSource);
-		break;
-	case USPI_LOG_NOTICE:
-		printf("\n NOTICE %s: - ", pSource);
-		break;
-	case USPI_LOG_DEBUG:
-		printf("\n DEBUG %s: - ", pSource);
-		break;
-	}
+    switch (Severity)
+    {
+    case USPI_LOG_ERROR:
+        printf("\n ERROR %s: - ", pSource);
+        break;
+    case USPI_LOG_WARNING:
+        printf("\n WARNING %s: - ", pSource);
+        break;
+    case USPI_LOG_NOTICE:
+        printf("\n NOTICE %s: - ", pSource);
+        break;
+    case USPI_LOG_DEBUG:
+        printf("\n DEBUG %s: - ", pSource);
+        break;
+    }
 
-	vprintf(pMessage, args);
-	va_end(args);
+    vprintf(pMessage, args);
+    va_end(args);
 }
 
 void MsDelay (unsigned nMilliSeconds)
 {
-	usDelay(1000 * nMilliSeconds);
+    usDelay(1000 * nMilliSeconds);
 }
 
 void usDelay (unsigned nMicroSeconds)
 {
-	TimeServer_sleep(TimeServer_PRECISION_USEC, nMicroSeconds);
+    OS_Error_t err;
+
+    if ((err = TimeServer_sleep(&timer, TimeServer_PRECISION_USEC,
+                                nMicroSeconds)) != OS_SUCCESS)
+    {
+        Debug_LOG_ERROR("TimeServer_sleep() failed with %d", err);
+    }
 }
 
-unsigned StartKernelTimer (unsigned nHzDelay, TKernelTimerHandler *pHandler, void *pParam, void *pContext)
+unsigned StartKernelTimer (unsigned nHzDelay, TKernelTimerHandler* pHandler,
+                           void* pParam, void* pContext)
 {
-	Debug_LOG_WARNING("Not implemented!");
-	return 0;
+    Debug_LOG_WARNING("Not implemented!");
+    return 0;
 }
 
 void CancelKernelTimer (unsigned hTimer)
 {
-	Debug_LOG_WARNING("Not implemented!");
+    Debug_LOG_WARNING("Not implemented!");
 }
 
 /*
 	Not necessary => used for configuring the usb interrupt
 	which is in our environment done by camkes
 */
-void ConnectInterrupt (unsigned nIRQ, TInterruptHandler *pHandler, void *pParam)
+void ConnectInterrupt (unsigned nIRQ, TInterruptHandler* pHandler, void* pParam)
 {
-	Debug_LOG_WARNING("Not implemented!");
+    Debug_LOG_WARNING("Not implemented!");
 }
 
 int SetPowerStateOn (unsigned nDeviceId)
 {
-	PropertyTagPowerState PowerState;
-	PowerState.nDeviceId = nDeviceId;
-	PowerState.nState = POWER_STATE_ON | POWER_STATE_WAIT;
+    PropertyTagPowerState PowerState;
+    PowerState.nDeviceId = nDeviceId;
+    PowerState.nState = POWER_STATE_ON | POWER_STATE_WAIT;
 
-	if (!MailboxInterface_getTag (PROPTAG_SET_POWER_STATE, &PowerState, sizeof PowerState, 8)
-	    || (PowerState.nState & POWER_STATE_NO_DEVICE)
-	    || !(PowerState.nState & POWER_STATE_ON))
-	{
-		Debug_LOG_ERROR("Failed to set power state on!");
-		return 0;
-	}
+    if (!MailboxInterface_getTag (PROPTAG_SET_POWER_STATE, &PowerState,
+                                  sizeof PowerState, 8)
+        || (PowerState.nState & POWER_STATE_NO_DEVICE)
+        || !(PowerState.nState & POWER_STATE_ON))
+    {
+        Debug_LOG_ERROR("Failed to set power state on!");
+        return 0;
+    }
 
-	return 1;
+    return 1;
 }
 
 int GetMACAddress (unsigned char Buffer[6])
 {
-	PropertyTagMACAddress MACAddress;
+    PropertyTagMACAddress MACAddress;
 
-	if(!MailboxInterface_getTag(PROPTAG_GET_MAC_ADDRESS, &MACAddress, sizeof(MACAddress), 0))
-	{
-		Debug_LOG_ERROR("Failed to retreive the MAC address!");
-		return 0;
-	}
+    if (!MailboxInterface_getTag(PROPTAG_GET_MAC_ADDRESS, &MACAddress,
+                                 sizeof(MACAddress), 0))
+    {
+        Debug_LOG_ERROR("Failed to retreive the MAC address!");
+        return 0;
+    }
 
-	memcpy(Buffer, MACAddress.Address, 6);
+    memcpy(Buffer, MACAddress.Address, 6);
 
-	return 1;
+    return 1;
 }
